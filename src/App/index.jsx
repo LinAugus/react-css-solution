e6d95a7ffba9dea6a85eccda11dbb4d50a0842c2
LinAugus <linaugus@gmail.com>
2017-11-27 00:46:41 +0800

    antd

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.less';
 import Home from '../page/Home';
+import Demo from '../page/Demo';
 
 class App extends Component {
   render() {
@@ -15,6 +21,8 @@ class App extends Component {
           Less + babel-react-css-modules + bem
         </p>
         <Home name="Hello React" />
+        <Link to="/demo">Go Antd</Link>
+        <Route path="/demo" component={Demo}/>
       </div>
     );
   }
